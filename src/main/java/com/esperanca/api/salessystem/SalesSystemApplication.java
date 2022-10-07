9908c9faefa72067df9b85f9efa03fe490583e60
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2022-10-07 17:50:40 -0300

    initial commit

---
+++ src/main/java/com/esperanca/api/salessystem/SalesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.esperanca.api.salessystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSystemApplication.class, args);
+	}
+
+}
